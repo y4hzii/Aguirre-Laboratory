1f18fdfc63f03a1ab28c1e1c46fd84256d161405
y4hzii <maui.aguirre25@gmail.com>
2025-04-13 17:56:29 +0800

    wangwang

--- Activity 1/vehicle.java
+++ Activity 1/vehicle.java
@@ -1,93 +1,133 @@
 import java.util.Scanner;
-
-public class vehicle {
+class Vehicle {
     private String brand;
-    private String fueltype;
     private int speed;
-    
+    private String fuelType;
 
-    //constructor
-    public vehicle(String brand, int speed, String fueltype) {
+    // Constructor
+    public Vehicle(String brand, int speed, String fuelType) {
         this.brand = brand;
         this.speed = speed;
-        this.fueltype = fueltype;
+        this.fuelType = fuelType;
     }
 
+    // Getter methods
     public String getBrand() {
         return brand;
-    }
-    
-    public String getfueltype() {
-        return fueltype;
     }
 
     public int getSpeed() {
         return speed;
     }
 
-    public void displayinfo() {
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    // Method to display info
+    public void displayInfo() {
         System.out.println("Brand: " + brand);
-        System.out.println("Fuel Type: " + fueltype);
         System.out.println("Speed: " + speed + " km/h");
-    }
-   
-    public static void main(String args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the brand of the vehicle: ");
-        String brand = sc.nextLine();
-        System.out.println("Enter the speed of the vehicle: ");
-        int speed = sc.nextInt();
-        System.out.println("Enter the fuel type of the vehicle: ");
-        String fueltype = sc.nextLine();
-        
-        vehicle v = new vehicle(brand, speed, fueltype);
-        v.displayinfo();
-    }
-
-    
-}
-
-class car extends vehicle {
-    private int doors;
-  
-
-    //constructor
-    public car(String brand, int speed, String fueltype, int doors) {
-        super(brand, speed, fueltype);
-        this.doors = doors;
-       ;
-    }
-
-    public int getDoors() {
-        return doors;
-    }
-
-
-    @Override
-    public void displayinfo() {
-        super.displayinfo();
-        System.out.println("Doors: " + doors);
+        System.out.println("Fuel Type: " + fuelType);
     }
 }
 
-class motorcycle extends vehicle {
-    private boolean hasSideca;
-  
+// Car Subclass
+class Car extends Vehicle {
+    private int numDoors;
 
-    //constructor
-    public motorcycle(String brand, int speed, String fueltype, String type) {
-        super(brand, speed, fueltype);
-        this.type = type;
+    public Car(String brand, int speed, String fuelType, int numDoors) {
+        super(brand, speed, fuelType);
+        this.numDoors = numDoors;
     }
-
-    public String getType() {
-        return type;
-    }
-
 
     @Override
-    public void displayinfo() {
-        super.displayinfo();
-        System.out.println("Type: " + type);
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Number of Doors: " + numDoors);
+    }
+}
+
+// Motorcycle Subclass
+class Motorcycle extends Vehicle {
+    private boolean hasSidecar;
+
+    public Motorcycle(String brand, int speed, String fuelType, boolean hasSidecar) {
+        super(brand, speed, fuelType);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Sidecar: " + (hasSidecar ? "Yes" : "No"));
+    }
+}
+
+// Main method
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean repeat = true;
+
+        System.out.println("VEHICLE MANAGEMENT SYSTEM");
+
+        while (repeat) {
+            System.out.print("\nWould you like to input a Car or Motorcycle? (car/motorcycle): ");
+            String type = scanner.nextLine().toLowerCase();
+
+            if (type.equals("car")) {
+                System.out.print("Enter Car Brand: ");
+                String brand = scanner.nextLine();
+
+                System.out.print("Enter Speed (km/h): ");
+                int speed = scanner.nextInt();
+                scanner.nextLine(); 
+
+                System.out.print("Enter Fuel Type: ");
+                String fuelType = scanner.nextLine();
+
+                System.out.print("Enter Number of Doors: ");
+                int doors = scanner.nextInt();
+                scanner.nextLine(); 
+
+                Car car = new Car(brand, speed, fuelType, doors);
+
+                System.out.println("\n--- Car Details ---");
+                car.displayInfo();
+
+            } else if (type.equals("motorcycle")) {
+                System.out.print("Enter Motorcycle Brand: ");
+                String brand = scanner.nextLine();
+
+                System.out.print("Enter Speed (km/h): ");
+                int speed = scanner.nextInt();
+                scanner.nextLine(); 
+
+                System.out.print("Enter Fuel Type: ");
+                String fuelType = scanner.nextLine();
+
+                System.out.print("Has Sidecar? (yes/no): ");
+                boolean hasSidecar = scanner.nextBoolean();
+                scanner.nextLine(); 
+
+                Motorcycle moto = new Motorcycle(brand, speed, fuelType, hasSidecar);
+
+                System.out.println("\nMOTORCYCLE DETAILS");
+                moto.displayInfo();
+
+            } else {
+                System.out.println("Invalid input. Please type 'car' or 'motorcycle'.");
+            }
+
+            System.out.print("\nDo you want to add another vehicle? (yes/no): ");
+            String again = scanner.nextLine().toLowerCase();
+
+            if (!again.equals("yes")) {
+                repeat = false;
+                System.out.println("Your use of the system is greatly appreciated!");
+            }
+        }
+
+        scanner.close();
     }
 }
